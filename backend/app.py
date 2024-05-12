d8ae97948d77d93b07730924cbe82585fab28338
TOM SABU <tomsabu0000@gmail.com>
2024-05-12 12:26:00 +0530

    CORS configuration changed to allow all origins and frontendvoice.wav deleted from audio directory.

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Allow CORS for all origins
 
 # Define your routes and corresponding functions
 @app.route('/')
